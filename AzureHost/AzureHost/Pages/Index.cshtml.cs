9dfda62749d549bd7192952cb371c3175193951e
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-12-29 19:13:29 -0700

    binary update

---
+++ AzureHost/AzureHost/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureHost.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Time { get; set; }
+
+        public void OnGet()
+        {
+            Time = DateTime.Now.ToLocalTime().ToShortTimeString();
+        }
+    }
+}
